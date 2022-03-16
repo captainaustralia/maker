0dcb9e426d398e25892cbf2508c5604c48bc08f7
mrnosomebody <73302906+mrnosomebody@users.noreply.github.com>
2022-03-16 23:59:48 +1000

    Update serializers.py

--- core/api/serializers.py
+++ core/api/serializers.py
@@ -6,5 +6,6 @@ from ..models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'email', 'phone', 'first_name', 'last_name', 'country', 'city', 'about']
+        fields = ['id', 'email', 'password', 'phone', 'first_name', 'last_name', 'country', 'city', 'about',
+                  'is_active']
         ordering = ['email']
